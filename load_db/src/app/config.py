71d9930254f1dfa1ec2acf52f333a8a073767939
newnativeabq <vincent.a.brandon@gmail.com>
2021-02-03 09:28:16 -0700

    ENH: created live config with shared tempfile

--- load_db/src/app/config.py
+++ load_db/src/app/config.py
@@ -1,6 +1,7 @@
 from decouple import config
 import os
 import logging
+import json
 
 
 def _mpath(*args) -> str:
@@ -12,37 +13,70 @@ def _mpath(*args) -> str:
         return t
 
 
-def _get_dburi()->str:
-    t = config('DBTYPE').upper()
-    return config(f'{t}_DBURI')
-
-VALIDATION = {
-    'SUPPORTED_DB': config('SUPPORTED_DB').split(',')
+DEFAULTS = {
+    'SUPPORTED_DB': config('SUPPORTED_DB').split(','),
+    'DEBUG': config('DEBUG'),
+    'DATADIR': config('DATADIR'),
+    'SQLDIR': config('SQLDIR'),
+    'QUERY_PLAN': config('QUERY_PLAN'),
 }
 
+# Live Config
+#   Create a config that read/writes to temporary file to allow multicomponent setting 
+#   without environment corruption.
+class Config():
+    def __init__(self, defaults=DEFAULTS):
+        self.store = {}
+        self.savepath = '/tmp/appsettings.json'
+        self.update(defaults)
+        
 
-DATABASE = {
-    'DBURI': _get_dburi(),
-    'DBTYPE': config('DBTYPE', default='postgres')
-}
+    def update(self, keyvals):
+        for key in keyvals:
+            self.store[key] = keyvals[key]
+        self.save()
+
+        if 'DBTYPE' in keyvals:
+            data_paths = self._gen_data_paths()
+            self.update(data_paths)
 
 
-def gen_data_paths(root:str, dbtype=None) -> dict:
-    if dbtype is None:
-        dbtype = config('DBTYPE')
-    d = {
-    'DATA': _mpath(
-        root),
-    'SQL': _mpath(
-        root, 
-        config('SQLDIR'), 
-        dbtype),
-    'QUERY_PLAN': _mpath(
-        root,
-        config('QUERY_PLAN'),
-        'query_plan.json'
-        )
-    }
-    return d
+    def save(self):
+        with open(self.savepath, 'w+') as f:
+            json.dump(self.store, f)
+    
 
-DATA = gen_data_paths(config('DATADIR'))
+    def read(self):
+        with open(self.savepath, 'r') as f:
+            self.update(json.load(f))
+
+
+    def __getitem__(self, idx):
+        self.read()
+        return self.store[idx]
+
+    
+    def __setitem__(self, key, val):
+        self.update({key:val})
+
+
+    def _gen_data_paths(self) -> dict:
+        dbtype = self.store['DBTYPE']
+        root = self.store['DATADIR']
+        sqldir = self.store['SQLDIR']
+        query_plan = self.store['QUERY_PLAN']
+        d = {
+        'DATA': _mpath(
+            root
+            ),
+        'SQL': _mpath(
+            root, 
+            sqldir, 
+            dbtype
+            ),
+        'QUERY_PLAN': _mpath(
+            root,
+            query_plan
+            ),
+        }
+        return d
